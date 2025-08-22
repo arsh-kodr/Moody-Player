ad711fa78f0d68332ef1d72f667e4db9ee83acd7
arsh-kodr <raisahab2364@gmail.com>
2025-08-22 17:16:18 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FacialExpression from './components/FacialExpression'
+
+const App = () => {
+  return (
+    <div>
+      <FacialExpression />
+    </div>
+  )
+}
+
+export default App
