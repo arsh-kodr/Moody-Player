067f711d51a8dc77a5e2b36e421db1cf6d7d680e
arsh-kodr <raisahab2364@gmail.com>
2025-08-24 16:37:24 +0530

    deployable

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const songRoutes = require("./routes/song.routes")
 const cors = require("cors")
+const path = require("path");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use("/" , songRoutes);
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
 
 module.exports = app; 
--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -1,45 +1,44 @@
 const express = require("express");
 const multer = require("multer");
-const uploadFile = require("../service/storage.service")
-const songModel = require("../models/song.model")
+const uploadFile = require("../service/storage.service");
+const songModel = require("../models/song.model");
+
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/songs", upload.single("audio") ,async (req, res) => {
+router.post("/songs", upload.single("audio"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
-  const fileData = await uploadFile(req.file); 
+  const fileData = await uploadFile(req.file);
   console.log(fileData);
 
   const song = await songModel.create({
-    title:req.body.title,
-    artist:req.body.artist,
-    audio:fileData.url,
-    mood:req.body.mood
-  })
-  
+    title: req.body.title,
+    artist: req.body.artist,
+    audio: fileData.url,
+    mood: req.body.mood,
+  });
+
   res.status(201).json({
     message: "Song Created Successfully",
     song: song,
   });
 });
 
-router.get("/songs" , async (req, res) => {
-    const {mood} = req.query;
+router.get("/songs", async (req, res) => {
+  const { mood } = req.query;
 
-    const songs = await songModel.find({
-      mood : mood
-    })
+  const songs = await songModel.find({
+    mood: mood,
+  });
 
-    res.status(200).json({
-      message : "Songs fetched successfully",
-      songs : songs
-    })
-})
+  res.status(200).json({
+    message: "Songs fetched successfully",
+    songs: songs,
+  });
+});
 
-router.get("*name" , (req , res) => {
-   
-} )
+
 
 module.exports = router;
